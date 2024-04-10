3e83796ed44b4035171220028e794e32793088a2
Sara <ilesbeksarah@gmail.com>
2024-04-10 14:16:02 +0500

    first commit

---
+++ Backend/ResQTails/src/main/java/com/example/ResQTails/ResQTailsApplication.java
@@ -0,0 +1,13 @@
+package com.example.ResQTails;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResQTailsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ResQTailsApplication.class, args);
+	}
+
+}
